8279be150d02ab000f83d6ad9ab3a6f2e4ff34cc
Gabriel Barros de Paula <gabriel_ciac6de@hotmail.com>
2020-01-29 08:56:27 -0300

    ngFor e ngIf, template interpolation e property binding.

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'jad-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  @Input() title: string = "";
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
